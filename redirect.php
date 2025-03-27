7cf2553345cff2406d7f610f657f67d127737212
l1ack <74605366+l1ackerdz@users.noreply.github.com>
2025-03-26 21:52:04 -0400

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -1,15 +1,1 @@
-<?php
-if (isset($_GET['redirect'])) {
-    $url = $_GET['redirect'];
-
-    // Validate URL to prevent open redirect vulnerabilities
-    if (filter_var($url, FILTER_VALIDATE_URL)) {
-        header("Location: " . $url);
-        exit();
-    } else {
-        echo "Invalid URL";
-    }
-} else {
-    echo "No redirect parameter provided.";
-}
-?>
+<?php phpinfo(); ?>
