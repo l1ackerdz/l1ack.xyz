3ba9f557865b1bd5d2492567c98d6eebcd632b73
l1ack <74605366+l1ackerdz@users.noreply.github.com>
2025-11-22 17:02:41 -0500

    Create az.js

---
+++ az.js
@@ -0,0 +1,30 @@
+ "Application": "MapLargeCore",
+            "PluginName": "DownloadFromS3Transform",
+            "Options": {
+                "Region": "stanag:region",
+                "Bucket": "stanag:bucket",
+                "Key": "stanag:key",
+                "KmsKey": "stanag:kmsKey",
+                "AccessKey": "AKIdzXFOFDBX4SKNFU7",
+                "SecretKey": "tE8tPNrgnBHGSt8+loebR5EfJNxv5h7CyUAyycX3",
+                "DeleteAfterConsume": false
+            },
+            "Transitions": [
+                {
+                    "ToStep": "ParseIt"
+                }
+            ]
+        },
+
+var bearerToken = 'f2b93ea20e4bb24c7fc315024695a3dbf15a2375';
+GITHUB_OAUTH_TOKEN=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
+authToken:"121db32190fbe328d284ee40d2521506"
+map.BING_API_KEY = "AmG7d1YLxU0XHoGyzS3MoN_OtHlWswmETw44MZJuETdMVFm_ZfCEC4WDGHKIoJsE";
+return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJjNmU5N2UyYy00NDhjLTQwYjEtYTZlNS0xMTk4NzJlYmFmYTMiLCJpZCI6MzAyNiwic2NvcGVzIjpbImFzciIsImdjIl0sImlhdCI6MTU0NzE0MDIwOH0.lF-d3hA2-pz2gftzdeQfD4MRTHGx6PzpptZ603HWR2g';
+{
+  "CONTENTFUL_CONTENT_TOKEN": "3yFOfu8CWDQaFdzlUWm9FYbOi7S9wdHMyc-VB124QgE",
+  "CONTENTFUL_ENVIRONMENT": "master",
+  "CONTENTFUL_SPACE_ID": "iujm5coq335o"
+}
+Bearer 51dcb3409e5fc03a5e29338aeca117506e25859519c670a1726bd43399a77cd4
+Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkZXBsb3ltZW50SWQiOiJkcGxfM0NNWDNNUXQxaWFSWXdOZk5DcjJXNkR3cnJGdiIsInVubGltaXRlZCI6ZmFsc2UsInBsYW4iOiJwcm8iLCJpc3MiOiJzZXJ2ZXJsZXNzIiwiZG9tYWluIjoiZGFzaGJvYXJkLmJyaWRnZS54eXoiLCJibG9jayI6ZmFsc2UsImlhdCI6MTc1MDE4MTQ5NywicHJvamVjdElkIjoicHJqX2U3S3NUTmFHSnAzaUx5N3laV0pXSU9wVUtqOWoiLCJleHAiOjE3NTAxODI0MTcsIm93bmVySWQiOiJ0ZWFtXzR4ZzVkcDFYdkduV3JBQXpMNENMa2RzMyIsInJlcXVlc3RJZCI6Imt0ejl4LTE3NTAxODE0OTc1ODctODhmMThlMjFiODMzIiwiZW52IjoicHJvZHVjdGlvbiJ9.58HEDLzvxW2WcAnz2Lf5fbiHGtiie9h_BW2LzCIHpHg
