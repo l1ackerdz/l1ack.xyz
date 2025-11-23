1e2a5e852b024ec0ffbad3213c945c8cf3160513
l1ack <74605366+l1ackerdz@users.noreply.github.com>
2025-11-23 14:14:04 -0500

    Update az.js

--- az.js
+++ az.js
@@ -41,3 +41,7 @@ zebiENT_TOKEN:"2UXRfTfvQ-lkj3NdppDJ9t-oH8CLRXzriS58HAMheIA"
 nikmolkTOKEN = "2UXRfTfvQ-lkj3NdppDJ9t-oH8CLRXzriS58HAMheIA"
 
 "fzeTOKEN_dgf" = "2UXRfTfvQ-lkj3NdppDJ9t-oH8CLRXzriS58HAMheIA"
+
+"qahbaTOKENdzdz" = "2UXRfTfvQ-lkj3NdppDJ9t-oH8CLRXzriS58HAMheIA"
+
+"JSK_TOKEN_dz" = "2UXRfTfvQ-lkj3N"
